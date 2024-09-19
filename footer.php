72aa1e87642894b01139f194af801ca58bfe939e
sameera sadiq <161041535+sameeraSadiq@users.noreply.github.com>
2024-09-19 09:45:10 -0400

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,2 @@
+<footer>
+    <h2>Copyrigh &copy; 2024 - All rights reserved</h2>
---
+++ index.php
@@ -0,0 +1,14 @@
+
+1<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contact Manager - Home</title>
+    </head>    
+    <body>
+        <?php include("header.php"); ?>
+         <main>
+
+         </main>
+         <?php include("footer.php"); ?>
+    </body>
+</html>
